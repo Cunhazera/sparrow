bff2893e779f7038327e8852e4c3d0e3286ae03f
cunhazera <cunha.gabriel919@gmail.com>
2016-03-21 00:26:39 -0300

    Starting project

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject sparrow "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  					[ring/ring-core "1.4.0"]
+                 	[ring/ring-jetty-adapter "1.4.0"]]
+
+
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler sparrow.core/handler}
+
+  :main ^:skip-aot sparrow.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
